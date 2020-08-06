4344c2ded713f82a416d98e5d7211b02f4c93540
Max Zavaleta Villar <TP33430229@oechsle.pe>
2020-08-05 21:03:28 -0500

    Change boolean type

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req,res)=>{
 app.get('/json', (req,res)=>{
     res.json({
         from:stage,
-        status:"OK",
+        status:true,
         data:"Hola Mundo",
         timestamp: new Date()
     })
